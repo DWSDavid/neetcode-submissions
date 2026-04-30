255717086847eaeff9cfd817880341aeae638c83
DWS <97680917+DWSDavid@users.noreply.github.com>
2026-05-01 01:57:30 +0800

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 跳过重复的i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            // 最小三数之和已经>0，后面不可能有解
+            if (nums[i] > 0) break;
+
+            int left = i + 1, right = nums.length - 1;//?
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 跳过重复的left和right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
